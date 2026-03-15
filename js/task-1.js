86bb3422cfbb90d9a3599cb6cdc369f33e4b469f
masya <marinabondarska2006@gmail.com>
2026-03-15 16:15:32 +0100

    chage js1

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,13 @@
 const categories = document.querySelector('#categories');
-const allItems = document.querySelectorAll('#categories .categories-list__item');
+const allItems = document.querySelectorAll('.item');
 
 console.log('Number of categories: ' + allItems.length);
 
 for(const item of allItems){
-    const title = item.querySelector('.categories-list__title');
-    console.log('Category: ' + title.textContent);
     
-    const elements = item.querySelectorAll('.categories-list__subitem');
+    const title = item.querySelector('h2');
+    console.log('Category: ' + title.textContent);
+    const elements = item.querySelectorAll('li');
     console.log('Elements: ' + elements.length);
+    
 }

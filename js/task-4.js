8f6eb4ea9d200d46a59ab3515c7b1f112c1cb9be
masya <marinabondarska2006@gmail.com>
2026-03-15 15:41:04 +0100

    js 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    
+    const { email, password } = event.target.elements;
+    
+    const trimmedEmail = email.value.trim();
+    const trimmedPassword = password.value.trim();
+    
+    if (trimmedEmail === '' || trimmedPassword === '') {
+        alert('All form fields must be filled in');
+        return;
+    }
+    
+    console.log({ email: trimmedEmail, password: trimmedPassword });
+    event.target.reset();
+});
+
+
 

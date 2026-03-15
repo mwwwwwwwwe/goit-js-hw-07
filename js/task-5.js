f41bf8c1954a2aeed2d0d9bb9f4c5059a0f69fb5
masya <marinabondarska2006@gmail.com>
2026-03-15 16:08:05 +0100

    made js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const randColorBtn = document.querySelector('.change-color');
+const colorSpan = body.querySelector('.color');
+
+randColorBtn.addEventListener('click', event => {
+  const color = getRandomHexColor();
+  body.setAttribute('style',`background-color:${color}`);
+  colorSpan.textContent = color;
+});
